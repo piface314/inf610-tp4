cae5f1ed18747c06c1bec8bbac60bc415e69b9f6
Henrique Santana <henriquessantana314@gmail.com>
2023-06-14 22:41:04 -0300

    Removed macro

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -4,14 +4,6 @@
 
 #include <ostream>
 
-#define PRINT_M(A,M,N) {std::cout << "\u23A1"; \
-    for (size_t i = 0; i < M; ++i) { \
-        std::cout << (i ? " " : "") << " "; \
-        for (size_t j = 0; j < N; ++j) \
-            std::cout << A[i][j] << " "; \
-        if (i + 1 == M) std::cout << "\u23A6"; \
-        std::cout << "\n"; \
-    }}
 
 template <typename T>
 struct Matrix {
